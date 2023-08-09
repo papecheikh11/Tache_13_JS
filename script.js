c23a8e86ec7e3f393bd6a7d93d11c708307b3798
Pape Maguette Ndiaye <papecheikh1102@gmail.com>
2023-08-09 21:33:28 +0000

    modifier mdp

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.getElementById("container");
 const loginButton = document.getElementById("login");
 const signUpButton = document.getElementById("signUp");
+const motDePasse = document.getElementById("motDePasse");
 
 signUpButton.addEventListener("click", () => {
   container.classList.add("panel-active");
@@ -8,4 +9,13 @@ signUpButton.addEventListener("click", () => {
 
 loginButton.addEventListener("click", () => {
   container.classList.remove("panel-active");
+});
+
+motDePasse.addEventListener("mouseout", (e) => {
+  contenu = e.target.value;
+  if (contenu.length <= 6) {
+    alert("entrer un mot de passe superieur a 6 caractéres");
+  } else {
+    console.log("valide");
+  }
 });
